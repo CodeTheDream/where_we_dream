8813c67532eaa3e4d350b0e7172ae63e2e4bab59
Cruz Nunez <nunez.a.cruz@gmail.com>
2015-08-28 15:57:02 -0400

    Add options to header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
@@ -21,5 +9,17 @@ function responsiveCoverHeight() {
   $( window ).resize( setCoverHeight );
 };
 
+function toggleMobileNav() {
+  $('#stack').click( function() {
+    if ($('#mobile-nav').css('display') == 'none') {
+      $('#mobile-nav').show()
+    } else {
+      $('#mobile-nav').hide()
+    };
+    $('#stack').css('color','black')
+  });
+};
+
 $(setCoverHeight);
 $(responsiveCoverHeight);
+$(toggleMobileNav)
