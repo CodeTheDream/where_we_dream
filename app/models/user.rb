afa90d4c5764e30378e42db70dcdc6813292c9d8
Cruz Nunez <nunez.a.cruz@gmail.com>
2015-10-26 23:36:27 -0400

    Add title to students page

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   def self.search(search)
     if search
       search_length = search.split.length
-      array = ((search.split*3).sort).map{ |term| term[/public|private/i] ? term[/public/i] ? true : false : "%#{term}%"}
+      array = ((search.split*3).sort).map{ |term| "%#{term}%"}
       where([(['first_name LIKE ? OR last_name LIKE ? OR user_type LIKE ?'] * search_length).join(' AND ')] + (array))
     else
       all
